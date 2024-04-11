090f5664cb7002748faf0d7ad1392df9bd3c8bda
João <nem878787@gmail.com>
2024-04-11 15:07:01 -0300

    configuração concluida

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-import Slide from '../js/modulos/slide.js';
+import Slide from './slide.js';
 
 const slide = new Slide('.slide', '.slide-wrapper');
 slide.init();
 console.log(slide);
+
+slide.changeSlide(3)
