cd454df8f90cbd43321654951b19abebec4e3ae4
João <nem878787@gmail.com>
2024-04-12 14:55:15 -0300

    finalizado potoes de anterior e seguinte

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-import Slide from './modulos/slide.js';
+import{ SlideNav }from './modulos/slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
+slide.addArrow('.prev', '.next');
+
 console.log(slide);
 
-slide.changeSlide(3);
-slide.activePrevSlide();
