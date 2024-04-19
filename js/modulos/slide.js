c667ac3c8ddc0ad25665d005c2918cbddc2d513d
João <nem878787@gmail.com>
2024-04-18 21:36:55 -0300

    controle navegação feito

--- js/modulos/slide.js
+++ js/modulos/slide.js
@@ -1,4 +1,4 @@
-import debounce from './debounce.js';
+import debounce from '../modulos/debounce.js';
 
 export class Slide {
   constructor(slide, wrapper) {
@@ -146,7 +146,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
     this.bindControlEvents();
